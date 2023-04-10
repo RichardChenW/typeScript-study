3ba0e59b1d0268ae960aec378572e655c5417840
richard-git <402822848@qq.com>
2023-04-10 11:02:43 +0800

    c1 type study

---
+++ C01-type/c1-01-hello.ts
@@ -0,0 +1,17 @@
+// 声明变量a,同时指定它的类型为 number
+let a:number;
+
+a = 10;
+// a = "richard"; //! 此行代码会报错，不能赋值字符串
+
+let b:unknown;
+b = "richard";
+
+let c:boolean = true;
+
+function sum(a:number,b:number):number{
+    return a + b;
+}
+
+console.log(sum(1,2));
+console.log(sum(1,"2"));
